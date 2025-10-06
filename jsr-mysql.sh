43a229a312510a5cfd221ab552abb040ab32a50d
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-06 09:04:56 -0400

    script addded/file added

--- jsr-mysql.sh
+++ jsr-mysql.sh
@@ -52,19 +52,11 @@ VALIDATE $? "Setting MySQL root password"
 
 mysql -h mysql.jsrdaws.online -u root -pExpenseApp@1 -e "show databses;"
 
-if [$? -ne 0]"
+if [$? -ne 0]
 then
 echo "MySQL Root password not setup" &>>$LOG_FILE_NAME
 mysql_secure_installation --set-root-pass ExpenseApp@1
 VALIDATE $? "Setting MySQL root password"
 else
-echo "MySQL Root password already setup.. $GREEN skipping$Normal"
+echo "MySQL Root password already setup.. $GREEN skipping $Normal"
 fi
-
-
-
-
-
-
-
-
