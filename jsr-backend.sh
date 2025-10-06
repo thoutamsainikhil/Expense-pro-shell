65e6003a0e8f858ce519f6f4b72e361cd7cb48ed
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-06 13:36:56 -0400

    script addded/file added

--- jsr-backend.sh
+++ jsr-backend.sh
@@ -64,7 +64,7 @@ fi
 mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating /app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "Downloading backend code"
 
 cd /app &>>$LOG_FILE_NAME
