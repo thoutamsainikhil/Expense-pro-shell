f10380182e21133d9184a7b541c598bc87021cbf
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-10-06 16:05:28 -0400

    script addded/file added

--- frontend.sh
+++ frontend.sh
@@ -1,56 +1,49 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-
 RED="\e[31m"
 GREEN="\e[32m"
-Normal="\e[0m"
+NORMAL="\e[0m"
 
 LOGS_FOLDER="/var/log/Expense-pro-shell-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$(basename "$0" | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-VALIDATE(){
-if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
+VALIDATE() {
+  if [ $1 -ne 0 ]; then
+    echo -e "${RED}$2 ... FAILURE${NORMAL}"
+    exit 1
+  else
+    echo -e "${GREEN}$2 ... SUCCESS${NORMAL}"
+  fi
 }
 
-
-
-
-
-echo "Script started executing at : $TIMESTAMP" &>>$LOG_FILE_NAME
-
-CHECK_ROOT(){
-if [ $USER_ID -ne 0 ]
-then
-  echo -e "$RED ERROR: You must be root user to run this script"
-  exit 1
-fi
+CHECK_ROOT() {
+  if [ $USER_ID -ne 0 ]; then
+    echo -e "${RED}ERROR: You must be root user to run this script${NORMAL}"
+    exit 1
+  fi
 }
 
+# Ensure script runs as root
 CHECK_ROOT
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at : $TIMESTAMP" &>>$LOG_FILE_NAME
 
-dnf install nginx -y &>>$LOG_FILE_NAME
+# Install and configure Nginx
+sudo dnf install nginx -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing Nginx Web Server"
 
-systemctl enable nginx &>>$LOG_FILE_NAME
+sudo systemctl enable nginx &>>$LOG_FILE_NAME
 VALIDATE $? "Enabling Nginx Service"
 
-systemctl start nginx &>>$LOG_FILE_NAME
-VALIDATE $? "Starting Nginx Service"    
+sudo systemctl start nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Nginx Service"
 
-rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
-VALIDATE $? "Removing existing Nginx HTML files"    
+sudo rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
+VALIDATE $? "Removing existing Nginx HTML files"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "Downloading frontend files"
@@ -58,12 +51,14 @@ VALIDATE $? "Downloading frontend files"
 cd /usr/share/nginx/html &>>$LOG_FILE_NAME
 VALIDATE $? "Changing directory to Nginx HTML folder"
 
-unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
-VALIDATE $? "Extracting frontend files" 
+sudo unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Extracting frontend files"
 
-cp /home/ec2-user/Expense-pro-shell/expense.conf /etc/nginx/conf.d/expense.conf &>>$LOG_FILE_NAME
+sudo cp /home/ec2-user/Expense-pro-shell/expense.conf /etc/nginx/conf.d/expense.conf &>>$LOG_FILE_NAME
 VALIDATE $? "Copying Nginx configuration file"
 
-systemctl restart nginx &>>$LOG_FILE_NAME
-VALIDATE $? "Restarting Nginx Service"
+sudo nginx -t &>>$LOG_FILE_NAME
+VALIDATE $? "Validating Nginx configuration"
 
+sudo systemctl restart nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Restarting Nginx Service"
